71dcb8231b9be9d9f892d6a40d19403dc7271c08
rest24 <restu.andriyana.suhendar@gmail.com>
2023-07-21 15:52:26 +0700

    tambahkeun

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const taneuh = document.querySelectorAll(".taneuh");
 const oray = document.querySelectorAll(".oray");
+const papanskor = document.querySelector(".papan-skor");
 
 let taneuhSebelumnya;
+let selesai = false;
+let skor = 0;
+let intervalId; // Deklarasikan variabel intervalId di luar fungsi mulai()
 
 function randomTaneuh(taneuh) {
   const t = Math.floor(Math.random() * taneuh.length);
   const tRandom = taneuh[t];
 
-  if (tRandom == taneuhSebelumnya) {
-    randomTaneuh(taneuh);
+  if (tRandom === taneuhSebelumnya) {
+    return randomTaneuh(taneuh);
   }
 
   taneuhSebelumnya = tRandom;
@@ -19,14 +23,39 @@ function randomWaktu(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-function munculkanOray(taneuh) {
+function munculkanOray() {
   const tRandom = randomTaneuh(taneuh);
   const wRandom = randomWaktu(300, 1000);
 
   tRandom.classList.add("muncul");
   setTimeout(() => {
     tRandom.classList.remove("muncul");
-    munculkanOray(taneuh);
   }, wRandom);
-  
+
+  if (!selesai) {
+    intervalId = setTimeout(munculkanOray, wRandom + 500); // Interval antara muncul dan hilang oray
+  }
 }
+
+function mulai() {
+  selesai = false;
+  skor = 0;
+  papanskor.textContent = 0;
+  munculkanOray();
+  setTimeout(() => {
+    selesai = true;
+    clearTimeout(intervalId); // Hentikan interval setelah waktu permainan berakhir
+  }, 10000);
+}
+
+function pukul() {
+  if (!selesai) {
+    skor++;
+    this.parentNode.classList.remove('muncul');
+    papanskor.textContent = skor;
+  }
+}
+
+oray.forEach(t => {
+  t.addEventListener('click', pukul);
+});
